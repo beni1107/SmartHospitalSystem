1d6820d0bffec1251c9f1c4ac0ad32998adae7fa
Beni <bendzy.thx@gmail.com>
2026-01-30 18:08:45 +0100

    First commit

---
+++ src/main/java/com/beni/smarthospitalsystem/SmartHospitalSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.beni.smarthospitalsystem;
+
+/**
+ *
+ * @author beni
+ */
+public class SmartHospitalSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
